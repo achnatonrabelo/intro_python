54f88c9012b1ea4b73605c6ae64a8ee363eb3cde
Achnaton Rabelo <achnaton.rabelo@gmail.com>
2025-06-06 13:54:43 -0400

    Definição do corpo da função get_time em tabuada/helpers.py

--- tabuada/helpers.py
+++ tabuada/helpers.py
@@ -1,8 +1,11 @@
 import os
 
+import datetime
+
 def clear_screen():
     """Limpa a tela do terminal."""
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def get_time():
-    pass
+def get_time() -> datetime:
+    agora = datetime.datetime.now()
+    return agora
